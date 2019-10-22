676731f3ccac3781e8db44b2fd2e92409e67e6c0
张麟 <zlinxx@foxmail.com>
2019-10-23 03:55:21 +0800

    Get album  add

--- controllers/album_controller.go
+++ controllers/album_controller.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"gin_blogweb/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func AlbumGet(c *gin.Context){
 	isLogin := GetSession(c)
+	albums,_ := models.FindAllAlbums()
 
-	c.HTML(http.StatusOK, "album.html", gin.H{"IsLogin": isLogin})
+	c.HTML(http.StatusOK, "album.html", gin.H{"IsLogin": isLogin, "Albums": albums})
 }
--- models/album_model.go
+++ models/album_model.go
@@ -3,14 +3,24 @@ package models
 import "gin_blogweb/database"
 
 type Album struct {
-	Id         int
-	FilePath   string
-	Filename   string
-	status     int
-	CreateTime int64
+	Id         int    `db:"id""`
+	FilePath   string `db:"filepath"`
+	Filename   string `db:"filename"`
+	Status     int    `db:"status"`
+	CreateTime int64  `db:"createtime"`
 }
 
-func InsertAlbum(album Album) (int64, error){
+func InsertAlbum(album Album) (int64, error) {
 	return database.ModifyDB("insert into album(filepath, filename, status, createtime) values(?,?,?,?)",
-		album.FilePath, album.Filename, album.status, album.CreateTime)
+		album.FilePath, album.Filename, album.Status, album.CreateTime)
+}
+
+func FindAllAlbums() (album []Album, err error) {
+	sqlStr := "select id, filepath, filename, status, createtime"
+	err = db.Select(&album, sqlStr)
+	if err != nil {
+		return nil, err
+	}
+	return album, err
+
 }
